4eb40abf07d6a7f55f46620ef4fe4ae585a8676c
azapata <administraor@gmail.com>
2022-12-05 10:53:14 +0100

    Tailwind proyecto inicial

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'body-pattern': "url('./assets/img/pattern.png')",
+      },
+      colors: {
+        'azul-claro': "#37bcf9",
+        'azul-oscuro': "#0370b9"
+      }
+    },
+  },
+  plugins: [],
+}
